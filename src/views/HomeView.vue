6f324a3b628252ad166e00a3e5be6c266c611bc0
h-chenchen <luyb@xunzhaotech.com>
2024-11-02 15:26:28 +0800

    fix -修改首页基础配置

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,3 +1,12 @@
+<!--
+ * @Description:
+ * @Author: xunzhaotech
+ * @Email: luyb@xunzhaotech.com
+ * @QQ: 1525572900
+ * @Date: 2024-10-06 11:18:37
+ * @LastEditTime: 2024-10-26 10:03:01
+ * @LastEditors: xunzhaotech
+-->
 <script setup lang="ts">
 import TheWelcome from '../components/TheWelcome.vue'
 </script>
